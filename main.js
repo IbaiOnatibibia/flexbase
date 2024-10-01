907bf154c0ef393f8c71faa30a54b2b2ead7bdca
Ibai Oñatibia Garmendia <157810112+IbaiOnatibibia@users.noreply.github.com>
2024-10-01 08:24:20 +0000

    argazkiaren tamaina zehatza

--- main.js
+++ main.js
@@ -1,37 +1,38 @@
 window.onload = () => {
-let lista = [
-    {
-      "isbn": "1617293563",
-      "egilea": "Raoul-Gabriel Urma",
-      "data": "Nov 15, 2018",
-      "izenburua": "Modern Java in Action: Lambdas, streams, functional and reactive programming",
-      "filename": "8508261-M.jpg"
-    },
-  
-   {
-      "isbn": "9781617291302",
-      "egilea": "Benjamin Muschko",
-      "data": "Mar 09, 2014",
-      "izenburua": "Gradle in Action",
-      "filename": "8514400-M.jpg"
-    },
-  
-       {
-      "isbn": "1883601126",
-      "egilea": "Matt Welsh",
-      "data": "1995",
-      "izenburua": "The Linux bible",
-      "filename": "6764181-M.jpg"
-    },
-    
+ let lista = [
   {
-      "isbn": "9781617293290",
-      "egilea": "Dmitry Jemerov",
-      "data": "Feb 19, 2017",
-      "izenburua": "Kotlin in Action",
-      "filename": "8507716-M.jpg"
-    }
-  ]
+    "isbn": "1617293563",
+    "egilea": "Raoul-Gabriel Urma",
+    "data": "Nov 15, 2018",
+    "izenburua": "Modern Java in Action: Lambdas, streams, functional and reactive programming",
+    "filename": "8508261-M.jpg"
+  },
+
+ {
+    "isbn": "9781617291302",
+    "egilea": "Benjamin Muschko",
+    "data": "Mar 09, 2014",
+    "izenburua": "Gradle in Action",
+    "filename": "8514400-M.jpg"
+  },
+
+     {
+    "isbn": "1883601126",
+    "egilea": "Matt Welsh",
+    "data": "1995",
+    "izenburua": "The Linux bible",
+    "filename": "6764181-M.jpg"
+  },
+  
+{
+    "isbn": "9781617293290",
+    "egilea": "Dmitry Jemerov",
+    "data": "Feb 19, 2017",
+    "izenburua": "Kotlin in Action",
+    "filename": "8507716-M.jpg"
+  }
+];
+
  let ind =0;
  let argazkia = document.getElementById("argazkia");
  let izenburua = document.getElementById("izenburu_text");
@@ -60,6 +61,7 @@ let lista = [
     egilea.value=lista[ind].egilea
     ISBN.value=lista[ind].isbn
     data.value=lista[ind].data
+    
   }
   ezkerr_botoi.addEventListener("click",left);
   eskuin_botoi.addEventListener("click", right);
